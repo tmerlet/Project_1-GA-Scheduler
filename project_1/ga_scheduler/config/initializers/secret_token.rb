cead699372b04379c18cabcb2c37bb2db6847739
tmerlet <tmerlet@gmail.com>
2014-07-28 14:38:41 +0100

    Initial planning and scheleton app

---
+++ project_1/ga_scheduler/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GaScheduler::Application.config.secret_token = '91c87f9384f7d85c14130ea9beb32dd1c8d4e94e6a9ce3c0ab7802a0e329693d501cb5e0ca088f7b1c450ccd809aa200a626002e9f291e0beda246af48b3efb8'
